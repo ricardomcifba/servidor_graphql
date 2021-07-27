dbc2b34afc4b4f0bc7149d6aaf9e4e19a87762db
Ricardo Martins <ricardomc.ifba@gmail.com>
2021-07-27 11:03:02 -0300

    GraphQL Server

---
+++ Readme.md
@@ -0,0 +1,3 @@
+Projeto de criação de Servidor com Uso de GraphQL
+
+Inicialmente criado um servidor, com uma rota e status.
---
+++ src/main.js
@@ -0,0 +1,32 @@
+import { createServer } from 'http';
+
+const server = createServer((request, response) => {
+    switch (request.url){
+        case '/status' : {
+            response.writeHead(200, {
+                'Content-Type': 'aplication/json',
+            });
+            response.write(
+                JSON.stringify({
+                    status:'Okay',
+                })
+            );
+            response.end();
+            return;
+        }
+        default:{
+            response.writeHead(404, 'Server not find.');
+            response.end();
+            return;
+        }
+
+    }
+    
+});
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
+
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server is listening at http://${HOSTNAME}:${PORT}.`);
+});
